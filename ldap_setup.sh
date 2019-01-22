2cadb58e19233eda2a208dab94e2bbd853b13232
Francisco Troncoso Pastoriza <ftroncoso@uvigo.es>
2019-01-22 17:58:24 +0100

    Fixes and included optional group for sudoers.

--- ldap_setup.sh
+++ ldap_setup.sh
@@ -8,13 +8,19 @@ apt install -y libnss-ldap libpam-ldap ldap-utils nscd
 # Basic configuration
 sed -i 's/\(\(passwd\|group\):.*\)/\1 ldap/g' /etc/nsswitch.conf
 sed -i 's/\(pam_ldap\.so.*\)\( use_authtok\)/\1/g' /etc/pam.d/common-password
-echo 'session optional        pam_mkhomedir.so skel=/etc/skel umask=077' >> /etc/pam.d/common-session
+echo 'session optional\tpam_mkhomedir.so skel=/etc/skel umask=077' >> /etc/pam.d/common-session
 
 # Shell and TLS configuration
 cfg='nss_override_attribute_value loginShell /bin/bash
 ssl start_tls
 tls_checkpeer no'
-echo -e "$cfg" >> /etc/ldap.conf
+echo "$cfg" >> /etc/ldap.conf
+
+# Add sudoers group
+read -p "sudo group (empty to omit): " sugroup
+if [ -z "$sugroup" ]; then
+  echo "%$sugroup\tALL=(ALL) ALL" >> /etc/sudoers
+fi
 
 # Restart daemons
 systemctl restart systemd-logind libnss-ldap nscd
